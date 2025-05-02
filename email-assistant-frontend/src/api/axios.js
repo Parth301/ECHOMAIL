c3a2bc956dcd839cc850f698bcc4a16d9107f2a5
Parth <127748701+Parth301@users.noreply.github.com>
2025-05-02 20:12:06 +0530

    Update axios.js

--- email-assistant-frontend/src/api/axios.js
+++ email-assistant-frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://echomail-backend-finale.vercel.app";
+const API_BASE_URL = "https://echomail-one.vercel.app";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
