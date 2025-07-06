5dccbc5a25fbc68a4b8308e8634e526be0ae1cff
Parth <127748701+Parth301@users.noreply.github.com>
2025-07-06 17:30:43 +0530

    Update axios.js

--- email-assistant-frontend/src/api/axios.js
+++ email-assistant-frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://echomail-izfm.onrender.com";
+const API_BASE_URL = "https://echomail-sfru.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
